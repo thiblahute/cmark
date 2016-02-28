9964014fd523fe83ae55fd87a519ebcc177fd180
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-02-28 20:47:26 +0100

    Implement and expose block parsing API.
    + Document the parser structure for internal usage.

--- src/blocks.c
+++ src/blocks.c
@@ -1215,3 +1215,39 @@ cmark_node *cmark_parser_finish(cmark_parser *parser) {
 #endif
   return parser->root;
 }
+
+int cmark_parser_get_line_number(cmark_parser *parser) {
+  return parser->line_number;
+}
+
+bufsize_t cmark_parser_get_offset(cmark_parser *parser) {
+  return parser->offset;
+}
+
+bufsize_t cmark_parser_get_column(cmark_parser *parser) {
+  return parser->column;
+}
+
+cmark_bufsize_t cmark_parser_get_first_nonspace(cmark_parser *parser) {
+  return parser->first_nonspace;
+}
+
+cmark_bufsize_t cmark_parser_get_first_nonspace_column(cmark_parser *parser) {
+  return parser->first_nonspace_column;
+}
+
+int cmark_parser_get_indent(cmark_parser *parser) {
+  return parser->indent;
+}
+
+bool cmark_parser_is_blank(cmark_parser *parser) {
+  return parser->blank;
+}
+
+bool cmark_parser_has_partially_consumed_tab(cmark_parser *parser) {
+  return parser->partially_consumed_tab;
+}
+
+cmark_bufsize_t cmark_parser_get_last_line_length(cmark_parser *parser) {
+  return parser->last_line_length;
+}
